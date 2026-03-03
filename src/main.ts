e2fff7aa21fdebdc93d52913e850e60960cca74a
Chasel <arko9988b@gmail.com>
2026-03-03 15:11:26 +0800

    feat: setup global route prefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from './libs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global prefix
+  app.setGlobalPrefix(`api`);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'v',
+  });
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('APP_PORT'));
